79b64ad619e503778f900950e796c9cf0ec3822a
jienba <adamjienba@outlook.fr>
2021-05-18 22:39:18 +0000

    road to master js

---
+++ 06-Modal/script.js
@@ -0,0 +1,29 @@
+'use strict';
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal )
+
+document.addEventListener('keydown', function (ev) {
+    if (ev.key === 'Escape' && !modal.classList.contains('hidden'))
+        closeModal()
+})
+
+
+
