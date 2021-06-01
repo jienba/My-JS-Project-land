ac9249b018f6ac5b36074a2800771986059eb9fd
jienba <adamjienba@outlook.fr>
2021-06-01 09:23:51 +0000

    12.Optional chaining done

--- 09-Data-Structures-Modern-Operators-and-Strings/script.js
+++ 09-Data-Structures-Modern-Operators-and-Strings/script.js
@@ -2,57 +2,88 @@
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    // Third enhancement with computing the name of property
+    [weekdays[3 + 1]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+}
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
     },
-    fri: {
-      open: 11,
-      close: 23,
+    orderDelevery ({starterIndex = 1, mainIndex, time = '20:00', address}) {
+        console.log(`Ordered received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
     },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
+    orderPasta (ing1, ing2, ing3) {
+        console.log(`Here is delicious pasta with ${ing1}, ${ing2} and ${ing3}`)
     },
-  },
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-  },
-  orderDelevery: function ({ starterIndex = 1, mainIndex, time = '20:00', address}) {
-    console.log(`Ordered received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is delicious pasta with ${ing1}, ${ing2} and ${ing3}`)
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-    let message = `You've ordered pizza with ${mainIngredient}`;
-    for (let i = 0; i < otherIngredients.length; i++) {
-      if (i === otherIngredients.length - 1){
-        message += ` and ${otherIngredients.length - 1}`;
-      } else{
-        message += `, ${otherIngredients[i]}`;
-      }
+    orderPizza (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+        let message = `You've ordered pizza with ${mainIngredient}`;
+        for (let i = 0; i < otherIngredients.length; i++) {
+            if (i === otherIngredients.length - 1) {
+                message += ` and ${otherIngredients.length - 1}`;
+            } else {
+                message += `, ${otherIngredients[i]}`;
+            }
 
-      console.log(message);
+            console.log(message);
+        }
+
+        if (otherIngredients.length !== 0) {
+        }
     }
-
-    if (otherIngredients.length !== 0) {
-    }
-  }
 };
+
+//////////////////////////////////////////////////
+// 12. Optional chaining
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+    console.log(restaurant.openingHours.mon.open);
+//With optional chaining
+console.log(restaurant.openingHours.mon?.open)
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days ){
+    const open = restaurant.openingHours[day]?.open ?? 'closed';
+    if (open !== 'closed' ){
+        console.log(`On ${day} we open at ${open}`)
+    }else{
+        console.log(`Today is ${day} We are closed. See you next time`);
+    }
+}
+
+// On Method
+console.log(restaurant.order?.(0,2) || 'Method does not exists');
+console.log(restaurant.orderRissoto?.(0,1) || 'Method does not exists');
+// Arrays
+const users = [{name: 'jienba', email:'adamjienba@techon.sn'}];
+console.log(users[0]?.name ?? 'User array empty');
+// Traditional way
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
 
 //////////////////////////////////////////////////
 // Rest pattern
@@ -133,15 +164,18 @@ restaurant.orderPizza && restaurant.orderPizza('mashroom','spinach')
 
  */
 // Nullish coalescing
+/*
 restaurant.numGuest = 0
+const guest = restaurant.numGuest || 10
+console.log(guest)
 
-const guest1 = restaurant.numGuest ??  10;
-console.log(guest1);
-
-const guest2 = restaurant.numGuest || 10
-console.log(guest2);
+// nullish: null and undefined (NOT 0 OR '')
+const guestCorrect = restaurant.numGuest ?? 10;
+console.log(guestCorrect);
 
 
+
+ */
 
 
 ///////////////////////////////////////////////
@@ -209,6 +243,7 @@ restaurant.orderDelevery({
 /////////////////////////////////////////////
 // Destructuring Object
 /*
+
 const  {name, openingHours, categories} = restaurant;
 console.log(name);
 console.log(openingHours);
@@ -219,6 +254,9 @@ console.log(nameRestaurant);
 console.log(hours);
 console.log(tags);
 console.log('///////////////////////////////');
+
+ */
+/*
 // Default value
 const { menu = [], starterMenu: starters = []} = restaurant;
 
@@ -275,6 +313,116 @@ const [p = 'default', q = 'default', r = 'default'] = [3,4]
 console.log(p,q,r)
 
  */
+
+//Coding Challenge #1
+/*
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// 1
+const [players1, players2] = game.players;
+
+console.log(players1, players2);
+
+// 2
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+// 3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers)
+// 4
+const player1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(player1Final);
+// 5
+const {team1, x:draw, team2 }  = game.odds;
+console.log(team1, draw, team2);
+// or
+//const {odds: {team1, x: draw, team2}} = game;
+//6
+const printGoals = function (...scoredPlayer) {
+
+  // Own
+  const scored = game.scored;
+  for (let i = 0; i < scoredPlayer.length; i++) {
+    let numberOfGoals = 0
+    for (let j = 0; j < scored.length; j++) {
+      if (scoredPlayer[i] === scored[j]){
+        numberOfGoals++;
+      }
+    }
+    numberOfGoals > 0 ? console.log(`${scoredPlayer[i]} scored ${numberOfGoals} goal(s) `) : console.log(`${scoredPlayer[i]} have not scored`)
+
+  }
+  // Own
+   // Correct chanlllenge
+  console.log(`${scoredPlayer.length} were scored`);
+
+}
+printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels', 'jienba' ,'Kimmich');
+team1 < team2 && console.log('Team 1 is more likely to win');
+team2 < team1 && console.log('Team 2 is more likely to win');
+
+ */
+///////////////////////////////////////
+//10. Looping array with for of
+/*
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+//for (const item of menu) console.log(item)
+for (const [i, element] of menu.entries()){
+    console.log(`${i + 1} : ${element}`)
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
 
 
 
