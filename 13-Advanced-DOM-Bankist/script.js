7951997a8e45070fc735a546b9a901a7c252a02d
jienba <adamjienba@outlook.fr>
2021-07-09 09:23:57 +0000

    CRUD element

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -8,24 +8,62 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
+/////////////////////////////////////////////////
+// 5. Selecting, Creating  and Deleting element
+
+// Selecting
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+const allSections = document.querySelector('.section');
+console.log(allSections);
+const header = document.querySelector('.header');
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'))
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it! </button>';
+message.innerHTML = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it! </button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+header.before(message);
+header.after(message);
+
+// Deleting element
+
+document
+    .querySelector('.btn--close-cookie')
+    .addEventListener('click', function () {
+      message.remove();
+      // message.parentElement.removeChild(message)
+    })
+
