5a4b8746640687cc9d4e7a16c1621f04c773d6de
jienba <adamjienba@outlook.fr>
2021-07-14 08:46:51 +0000

    DOM traversing, using event event delegation and more to build tabbed component

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -136,7 +136,31 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
         const id = e.target.getAttribute('href');
         document.querySelector(id).scrollIntoView({behavior: "smooth"})
     }
-})
+});
+
+//////////////////////////////////////////////////
+// Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainers = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+console.log(tabs);
+tabsContainers.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.operations__tab');
+    // Guard clause
+    if (!clicked) return;
+
+    // Remove classes
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c=> c.classList.remove('operations__content--active'));
+    // Activate tab
+    clicked.classList.add('operations__tab--active');
+    // Activate content area
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add('operations__content--active');
+});
 
 
 
@@ -263,9 +287,37 @@ h1.onmouseenter = function (e) {
 //
 // })
 //
+//12. DOM Traversing
+/*
 
+const h1 = document.querySelector('h1');
 
+//Going downwards: child
+console.log(h1.querySelectorAll('.highlight'))
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
 
+//Going upwards: parent
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// Going sideways: siblings
+
+console.log(h1.previousSibling);
+console.log(h1.nextElementSibling);
+console.log('node', h1.previousSibling);
+console.log('node', h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+    if (el !== h1) el.style.transform = 'scale(0.5)'
+})
+
+*/
 
 
 
