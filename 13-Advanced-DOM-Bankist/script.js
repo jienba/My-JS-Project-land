c98b7262ef2ddb94eb3b1ebbb7af5909df772e8c
jienba <adamjienba@outlook.fr>
2021-07-28 13:21:00 +0000

    17. Implementing revealing sections with Intersection Observer API. This API becomes more and more clear. We can also for a root Element observe more than one target like we did in line 233 --> 236

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -214,6 +214,27 @@ headerObserver.observe(header);
     console.log(window.scrollY);
 });*/
 
+/////////////////////////////////////////////////
+// 17. Reveal sections
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    console.log();
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+
+    observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {root: null, threshold:0.1});
+
+allSections.forEach(section => {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+})
+
 
 
 
