5c6c648cea3c4a1b7d12ef0784b7152cc9dc197d
jienba <adamjienba@outlook.fr>
2021-08-09 15:52:51 +0000

    #9. Coding challenge 1 done!

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -33,3 +33,43 @@ ndeye.calcAge();
 Person.prototype.species = 'Homo Sapiens';
 
 console.log(jienba.species);
+
+console.log(jienba.__proto__);
+
+// Object.prototype (top of prototype chain)
+console.log(jienba.__proto__.__proto__);
+console.log(jienba.__proto__.__proto__.__proto__);
+
+// Coding Challenge #1
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h.`);
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h.`);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Ferrari', 95);
+const car3 = new Car('Lamborghini', 230);
+
+car1.accelerate();
+car2.brake();
+car3.accelerate();
+
+
+
+
+
+
+
+
+
+
