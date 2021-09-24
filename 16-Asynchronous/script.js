ad10e79981aa38c71e0c8f82917c30c9ce925cbb
jienba <adamjienba@outlook.fr>
2021-09-24 16:42:20 +0000

    Part-16: Asynchronous JS #8. Consuming promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -68,6 +68,36 @@ getCountryAndNeighbour('usa');
 /*const request = new XMLHttpRequest();
 request.open('GET', `https://restcountries.com/v2/alpha/${countryCode}`);
 request.send();*/
-const req = fetch('https://restcountries.com/v2/alpha/sn');
-console.log(req)
+/*const req = fetch('https://restcountries.com/v2/alpha/sn');
+console.log(req)*/
+
+
+//////////////////////////////////////////////////////////
+// #8. Consuming promises
+const renderCountry = function (data, className= '') {
+    const html = `
+         <article class="country ${className}">
+          <img class="country__img" src="${data.flags[0]}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.continent}</h4>
+            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} Millions</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0]['nativeName']}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
+
+const getCountryData = (countryCode) => {
+    fetch(`https://restcountries.com/v2/alpha/${countryCode}`)
+        .then(response => response.json())
+        .then(data => {
+            renderCountry(data);
+        })
+}
+
+getCountryData('sn');
 
