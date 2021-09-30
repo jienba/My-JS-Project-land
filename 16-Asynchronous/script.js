b3c7ff1be76eae616c367a05b464c34d7727eba2
jienba <adamjienba@outlook.fr>
2021-09-30 16:13:04 +0000

    Part-16: Asynchronous JS #14. The event loop in practice

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -232,6 +232,7 @@ fetch('https://restcountries.com/v2/alpha/sn')
 */
 //////////////////////////////////////////////////////
 // 12. Coding challenge #1
+/*
 const renderCountryByName = function (data, className= '') {
     const html = `
          <article class="country ${className}">
@@ -295,7 +296,41 @@ const getPosition = function (){
     )
 }
 getPosition();
-// whereAmI(52.508, 13.381);
-// whereAmI(19.037, 72.873);
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+/////////////////////////////////////////////
+// 14. The event loop in practice
+
+console.log('Test start');
+setTimeout(()=>console.log('0 sec timer'),0 );
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+Promise.resolve('Resolved promise 2').then(res =>{
+    setTimeout(()=> console.log(res), 2500)
+})
+console.log('Test end');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
